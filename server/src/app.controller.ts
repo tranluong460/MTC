6ae305f266eaeac10399c54937d652f9e7ee1851
Alone <tranluong460@gmail.com>
2024-05-04 22:18:25 +0700

    up

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './common/public.decorator';
+import { CheckAbilities } from './common/casl-ability.decorator';
+import { Action, Subjects } from './ability/casl-ability.factory';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
+  @CheckAbilities({ action: Action.Publish, subject: Subjects.Auth })
   @Get()
   base(): string {
     return this.appService.base();
