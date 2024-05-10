4a6c743db7f53f4a4e5c7db57057b680ff7c2f2c
Alone <tranluong460@gmail.com>
2024-05-10 07:29:26 +0700

    fix

--- client/app/(base)/truyen/page.tsx
+++ client/app/(base)/truyen/page.tsx
@@ -4,21 +4,21 @@ import { getCategoryBy } from "../../../libs/category";
 import { ICategory } from "../../../interface/category";
 import { IParams } from "../../../interface/params";
 
-export async function generateMetadata({
-  searchParams,
-}: IParams): Promise<Metadata> {
-  const { c, p } = searchParams;
+// export async function generateMetadata({
+//   searchParams,
+// }: IParams): Promise<Metadata> {
+//   const { c, p } = searchParams;
 
-  const category: ICategory = c ? await getCategoryBy({ slug: c }) : "";
-  const categoryName = category ? `- ${category.name} ` : "";
+//   const category: ICategory = c ? await getCategoryBy({ slug: c }) : "";
+//   const categoryName = category ? `- ${category.name} ` : "";
 
-  const pageNumber = p || 1;
+//   const pageNumber = p || 1;
 
-  return {
-    title: `Danh Sách Truyện Convert ${categoryName}- Mới cập nhật - Trang ${pageNumber}`,
-    description: `Trang ${pageNumber} - Danh sách truyện ${categoryName}- Mới cập nhật tại ${process.env.FRONTEND_URL}`,
-  };
-}
+//   return {
+//     title: `Danh Sách Truyện Convert ${categoryName}- Mới cập nhật - Trang ${pageNumber}`,
+//     description: `Trang ${pageNumber} - Danh sách truyện ${categoryName}- Mới cập nhật tại ${process.env.FRONTEND_URL}`,
+//   };
+// }
 
 const TimKiemTruyen = ({ searchParams }: IParams) => {
   return (
